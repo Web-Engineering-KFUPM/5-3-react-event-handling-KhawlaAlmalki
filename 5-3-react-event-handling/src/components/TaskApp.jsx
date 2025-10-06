7cb98ec3e6d535aa879a2aed997945765c973352
KhawlaAlmalki <s202247320@kfupm.edu.sa>
2025-10-06 11:00:40 +0300

    Task 1

--- 5-3-react-event-handling/src/components/TaskApp.jsx
+++ 5-3-react-event-handling/src/components/TaskApp.jsx
@@ -2,50 +2,50 @@ import React, { useState } from "react";
 import TaskList from "./TaskList";
 
 export default function TaskApp() {
-  
-  const handleSubmit = () => {
-   
-  };
+    // Task 1: controlled input
+    const [text, setText] = useState("");
 
-  
-  const handleDelete = (id) => {
-    // TODO: filter tasks by id to remove the clicked one
-  };
+    const handleSubmit = () => {
 
-  
-  const handleClearAll = () => {
-    // TODO: set tasks to empty array
-  };
+    };
 
-  return (
-    <section className="card">
-      {/*Controlled Input */}
-      <div className="inputRow">
-        <input
-          type="text"
-          placeholder="Type a task..."
-          className="input"
-          // TODO: value={text}
-          // TODO: onChange={(e) => setText(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") handleSubmit();
-          }}
-        />
-        <button className="btn btn--primary" onClick={handleSubmit}>
-          Submit
-        </button>
-      </div>
+    const handleDelete = (id) => {
+        // TODO: filter tasks by id to remove the clicked one
+    };
 
-      {/*Render Task List and Enable Delete */}
-      {/*Pass tasks and onDelete */}
-      <TaskList /* tasks={tasks} onDelete={handleDelete} */ />
+    const handleClearAll = () => {
+        // TODO: set tasks to empty array
+    };
+    return (
+        <section className="card">
+            {/* Controlled Input */}
+            <div className="inputRow">
+                <input
+                    type="text"
+                    placeholder="Type a task..."
+                    className="input"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") handleSubmit();
+                    }}
+                />
+                <button className="btn btn--primary" onClick={handleSubmit}>
+                    Submit
+                </button>
+            </div>
 
-      {/*Clear All */}
-      <div className="footerRow">
-        <button className="btn btn--ghost" onClick={handleClearAll}>
-          Clear All
-        </button>
-      </div>
-    </section>
-  );
+            <p>{text}</p>
+
+            {/* Render Task List and Enable Delete */}
+            <TaskList /* tasks={tasks} onDelete={handleDelete} */ />
+
+            {/* Clear All */}
+            <div className="footerRow">
+                <button className="btn btn--ghost" onClick={handleClearAll}>
+                    Clear All
+                </button>
+            </div>
+        </section>
+    );
 }
